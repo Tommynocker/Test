7ff59a92bdf21c0ef2b8e8ee28341ef8b09b75b0
Thomas Rakowski <thomas.rakowski@icloud.com>
2024-09-12 16:43:07 +0200

    test

--- Sources/App/Controllers/TestController.swift
+++ Sources/App/Controllers/TestController.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import Vapor
+import Fluent
+import OracleNIO
+
+struct TestController: RouteCollection {
+    
+    func boot(routes: RoutesBuilder) throws {
+        
+        let route = routes.grouped("api", "consumer")
+        
+        route.get("test", use: test)
+    }
+    
+    // post register
+    func test(req: Request) async throws -> String {
+        
+        // config oracle
+        let logger = Logger(label: "oracle-logger")
+        
+        let config = OracleConnection.Configuration(
+            host: "172.20.1.36",
+            port: 12002,
+            service: .serviceName("Test77"), // or .sid("sid")
+            username: "infor",
+            password: "sysm"
+        )
+        
+        let connection = try await OracleConnection.connect(
+          configuration: config,
+          id: 1,
+          logger: logger
+        )
+        
+        let rows = try await connection.execute("SELECT werkstoff,sollstk FROM infor.apltsaegen", logger: logger)
+        
+        
+        print(rows)
+        var x = "zonk"
+        for try await (werkstoff, sollstk) in rows.decode((String, Float).self) {
+          // do something with the datatypes.
+             x = werkstoff
+            let y = sollstk
+            print("")
+        }
+        
+        
+        return x
+    }
+     
+   
+    
+    
+}
+
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,7 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-        app.get { req async in
-        "It works! Version2"
-    }
 
-    app.get("hello") { req async -> String in
-        "Hello, world! Yes"
-    }
+func routes(_ app: Application) throws {
+    // register Controller
+    try app.register(collection: TestController())
 }
