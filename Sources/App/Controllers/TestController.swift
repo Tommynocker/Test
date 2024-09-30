b3d29e8ed0c95bb05c29646349f1e01d25a8b050
Thomas Rakowski <thomas.rakowski@icloud.com>
2024-09-30 08:54:51 +0200

    test

--- Sources/App/Controllers/TestController.swift
+++ Sources/App/Controllers/TestController.swift
@@ -9,6 +9,7 @@ import Foundation
 import Vapor
 import Fluent
 import OracleNIO
+import Logging
 
 
 struct TestController: RouteCollection {
@@ -42,21 +43,47 @@ struct TestController: RouteCollection {
           logger: logger
         )
         
-        let rows = try await connection.execute("SELECT * FROM infor.icast_cast_order", logger: logger)
+        let result = try await connection.execute("SELECT * FROM infor.icast_cast_order", logger: logger)
         
         var order = [OrderDTO]()
         
-//        for try await row in rows{
+     
+        
+        
+        let x = try await result.collect()
+        
+        x.forEach { ora in
+            print("")
+            let x  = ora.keyed { $0.columnName == "ANR"}.values
+           
+            print("")
+        }
+//
+//        for try await row in result.collect() {
+//            let x = try row.decode(String.self, forKey: "anr")
 //            
-//            let x = row.decode(OrderDTO.self)
-//            order.append(x)
+//            
+//            
+//        }
+    
+      
+        
+//        for row in try await result.collect() {
+//            
+//            if let anr = row["anr"] as? String {
+//                
+//            }
+//            
 //        }
         
         
-        for try await (anr) in rows.decode((String).self) {
-          // do something with the datatypes.
-            order.append(OrderDTO(anr: anr))
-        }
+        
+       
+//        let x = rows.map([OrderDTO].self)
+//        for try await (anr,artikel,werkst,fv,formnr,abc) in rows.decode((String,String,String,String,String,String).self) {
+//          // do something with the datatypes.
+//            order.append(OrderDTO(anr: anr, artikel: artikel, werkst: werkst, fv: fv, formnr: formnr, abc: abc, flgew: 0, status: nil, kw: nil, datum: nil, fis: nil))
+//        }
         
         // Close your connection once done
         try await connection.close()
--- Sources/App/Models/Order.swift
+++ Sources/App/Models/Order.swift
@@ -7,15 +7,36 @@
 
 import Foundation
 import Vapor
+import OracleNIO
 
 
 
 public struct OrderDTO: Codable, Content {
     
     public var anr: String?
+    public var artikel: String?
+    public var werkst: String?
+    public var fv: String?
+    public var formnr: String?
+    public var abc: String?
+    public var flgew: Int
+    public var status: String?
+    public var kw: String?
+    public var datum: String?
+    public var fis: String?
     
-    init(anr: String?) {
+    init(anr: String?, artikel: String?, werkst: String?, fv: String?, formnr: String?, abc: String?, flgew: Int,status: String?,kw: String?,datum: String?, fis: String?) {
         self.anr = anr
+        self.artikel = artikel
+        self.werkst = werkst
+        self.fv = fv
+        self.formnr = formnr
+        self.abc = abc
+        self.flgew = flgew
+        self.status = status
+        self.kw = kw
+        self.datum = datum
+        self.fis = fis
     }
 }
 
