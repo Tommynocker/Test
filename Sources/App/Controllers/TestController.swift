6423c8776757bfc949db68bff9174801aa74f2b5
Thomas Rakowski <thomas.rakowski@icloud.com>
2024-12-19 15:39:00 +0100

    test

--- Sources/App/Controllers/TestController.swift
+++ Sources/App/Controllers/TestController.swift
@@ -55,7 +55,7 @@ struct TestController: RouteCollection {
             
             var dataWXZ = [WXZDTO]()
             for try await data in stream {
-                dataWXZ.append(.init(mnr: data.mnr, werkstoff: data.werkstoff, staerke: data.staerke, stk_v: data.stk_v, kg_v: data.kg_v, stk_l: data.stk_l, kg_l: data.kg_l, bedarfe_stk: data.bedarfe_stk, verbr12_kg: data.verbr12_kg, ktxt: data.ktxt, bemerkung: data.bemerkung ?? "", variante: data.variante, gueltigkeit: data.gueltigkeit, verbrauchq1: data.verbrauchq1, verbrauchq2: data.verbrauchq2, verbrauchq3: data.verbrauchq3))
+                dataWXZ.append(.init(mnr: data.mnr, werkstoff: data.werkstoff, staerke: data.staerke, stk_v: data.stk_v, kg_v: data.kg_v, stk_l: data.stk_l, kg_l: data.kg_l, bedarfe_stk: data.bedarfe_stk, verbr12_kg: data.verbr12_kg, ktxt: data.ktxt, bemerkung: data.bemerkung ?? "", variante: data.variante, gueltigkeit: data.gueltigkeit, verbrauchq1: data.verbrauchq1, verbrauchq2: data.verbrauchq2, verbrauchq3: data.verbrauchq3, verbrauchq4: data.verbrauchq4))
             }
             print("")
             
@@ -69,12 +69,12 @@ struct TestController: RouteCollection {
             return dataWXZ
             
         } else {
-            let stream = try? await connection.execute("SELECT mnr, werkstoff,staerke,stk_v,kg_v,stk_l,kg_l,bedarfe_stk,verbr12_kg,ktxt,bemerkung,variante,gueltigkeit,verbrauchq1,verbrauchq2,verbrauchq3 FROM US_BLZ_ARTIKELKONTO_WXZ", logger: logger)
+            let stream = try? await connection.execute("SELECT mnr, werkstoff,staerke,stk_v,kg_v,stk_l,kg_l,bedarfe_stk,verbr12_kg,ktxt,bemerkung,variante,gueltigkeit,verbrauchq1,verbrauchq2,verbrauchq3,verbrauchq4 FROM US_BLZ_ARTIKELKONTO_WXZ", logger: logger)
             
             var dataWXZ = [WXZDTO]()
             if let stream = stream {
-                for try await (mnr, werkstoff, staerke, stk_v, kg_v, stk_l, kg_l, bedarfe_stk, verbr12_kg, ktxt, bemerkung, variante, gueltigkeit, verbrauchq1, verbrauchq2, verbrauchq3) in stream.decode((String, String, Float, Int, Int, Int, Int, Int, Int, String, String, String, String, Int, Int, Int).self) {
-                    dataWXZ.append(.init(mnr: mnr, werkstoff: werkstoff, staerke: staerke, stk_v: stk_v, kg_v: kg_v, stk_l: stk_l, kg_l: kg_l, bedarfe_stk: bedarfe_stk, verbr12_kg: verbr12_kg, ktxt: ktxt, bemerkung: bemerkung, variante: variante, gueltigkeit: gueltigkeit, verbrauchq1: verbrauchq1, verbrauchq2: verbrauchq2, verbrauchq3: verbrauchq3))
+                for try await (mnr, werkstoff, staerke, stk_v, kg_v, stk_l, kg_l, bedarfe_stk, verbr12_kg, ktxt, bemerkung, variante, gueltigkeit, verbrauchq1, verbrauchq2, verbrauchq3,verbrauchq4) in stream.decode((String, String, Float, Int, Int, Int, Int, Int, Int, String, String, String, String, Int, Int, Int,Int).self) {
+                    dataWXZ.append(.init(mnr: mnr, werkstoff: werkstoff, staerke: staerke, stk_v: stk_v, kg_v: kg_v, stk_l: stk_l, kg_l: kg_l, bedarfe_stk: bedarfe_stk, verbr12_kg: verbr12_kg, ktxt: ktxt, bemerkung: bemerkung, variante: variante, gueltigkeit: gueltigkeit, verbrauchq1: verbrauchq1, verbrauchq2: verbrauchq2, verbrauchq3: verbrauchq3, verbrauchq4: verbrauchq4))
                 }
             }
             
